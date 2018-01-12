8b8285f8f7cc51d49e109b9e3d66280989d06696
Vladimir Morulus <vladimirmorulus@gmail.com>
2018-01-12 19:42:45 +0300

    passive mode, functional value

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
+const throttle = require('lodash/throttle');
+
 function isArray(arrayLike) {
   return typeof arrayLike === 'object' &&
     arrayLike instanceof Array;
 }
 
-module.exports = function animate(frames, minDelay = 33, onFinish) {
-  if (!frames || !isArray(frames)) {
-    throw new TypeError('Frames must be an array');
+function isFunction(funcLike) {
+  return typeof funcLike === 'function';
+}
+
+function compile(frame) {
+  return typeof frame === 'function' ?
+    frame() : frame;
+}
+
+module.exports = function animate(frames, delay = 0, onFinish) {
+  if (!(frames && (isArray(frames) || isFunction(frames)))) {
+    throw new TypeError('Frames must be an array or a function');
   }
+  if (isFunction(frames)) {
+    return throttle(() => {
+      return frames();
+    }, delay)
+  }
+
   let i = -1;
   let initTime;
-  return () => {
-    const currentTime = new Date().getTime();
-    if (initTime === undefined) {
-      initTime = currentTime;
-    }
-    const diff = currentTime - initTime;
-    i = Math.round(diff / minDelay);
-
-    if (i >= frames.length) {
-      initTime = currentTime;
-      i = 0;
-      if (onFinish) {
-        onFinish(diff);
+  return delay
+    ? () => {
+      const currentTime = new Date().getTime();
+      if (initTime === undefined) {
+        initTime = currentTime;
       }
+      const diff = currentTime - initTime;
+      i = Math.round(diff / delay);
+
+      if (i >= frames.length) {
+        initTime = currentTime;
+        i = 0;
+        if (onFinish) {
+          onFinish(diff);
+        }
+      }
+      return compile(frames, i);
+    } : () => {
+      i++;
+      if (i >= frames.length) {
+        i = 0;
+      }
+      return compile(frames[i]);
     }
-    return typeof frames[i] === 'function' ?
-      frames[i]() : frames[i];
-  };
 };
